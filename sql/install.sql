aeded181b9f856b3f67f08150f4aedf69cb499bf
eileen <emcnaughton@wikimedia.org>
2017-08-29 09:40:17 +1200

    Add id field

--- sql/install.sql
+++ sql/install.sql
@@ -1,4 +1,5 @@
 CREATE TABLE `civicrm_merge_conflict` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Unique ID',
   `contact_1` int(10) unsigned NOT NULL COMMENT 'FK to entity table specified in entity_table column.',
   `contact_2` int(10) unsigned NOT NULL COMMENT 'FK to entity table specified in entity_table column.',
   `group_id` int(10) NOT NULL DEFAULT '0',
@@ -6,6 +7,7 @@ CREATE TABLE `civicrm_merge_conflict` (
   `value_1` varchar(255) NOT NULL DEFAULT '',
   `value_2` varchar(255) NOT NULL DEFAULT '',
   `analysis` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
   KEY `contact_1` (`contact_1`,`contact_2`),
   KEY `contact_2` (`contact_2`,`contact_1`),
   KEY `value_1` (`value_1`,`value_2`),
@@ -16,12 +18,14 @@ CREATE TABLE `civicrm_merge_conflict` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE `civicrm_merge_matches` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Unique ID',
   conflicted_field varchar(255) NOT NULL DEFAULT '',
   `value_1` varchar(255) NOT NULL DEFAULT '',
   `value_2` varchar(255) NOT NULL DEFAULT '',
   `probability` int(10) NOT NULL DEFAULT 0,
   `created_id` int(10) NULL,
   `created_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
   KEY `value_1` (`value_1`,`value_2`),
   KEY `value_2` (`value_2`,`value_1`),
   KEY `conflicted_field` (`conflicted_field`)
