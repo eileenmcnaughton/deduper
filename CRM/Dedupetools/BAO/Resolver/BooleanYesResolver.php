a42f6414ccef80f4f13c52415917816a79d3ff19
eileen <emcnaughton@wikimedia.org>
2019-10-23 14:08:12 +1300

    Cast to  an array to avoid notices

--- CRM/Dedupetools/BAO/Resolver/BooleanYesResolver.php
+++ CRM/Dedupetools/BAO/Resolver/BooleanYesResolver.php
@@ -11,8 +11,8 @@ class CRM_Dedupetools_BAO_Resolver_BooleanYesResolver extends CRM_Dedupetools_BA
    * Resolve conflicts if possible.
    */
   public function resolveConflicts() {
-    $fieldsToResolve = Civi::settings()->get('deduper_resolver_bool_prefer_yes');
-    $conflictedFields = $this->getFieldsInConflict();
+    $fieldsToResolve = (array) Civi::settings()->get('deduper_resolver_bool_prefer_yes');
+    $conflictedFields = (array) $this->getFieldsInConflict();
     $fieldsAffected = array_intersect($fieldsToResolve, $conflictedFields);
     foreach ($fieldsAffected as $field) {
       $this->setResolvedValue($field, 1);
