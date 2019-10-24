fa3915c060479e434bf5e7fe35dab81a30a43b4b
eileen <emcnaughton@wikimedia.org>
2019-10-24 17:15:53 +1300

    Add dedupe resolver on yes-no fields for batch mode

--- tests/phpunit/CRM/DedupeTools/BAO/MergeConflictTest.php
+++ tests/phpunit/CRM/DedupeTools/BAO/MergeConflictTest.php
@@ -26,6 +26,16 @@ class CRM_DedupeTools_BAO_MergeConflictTest extends DedupeBaseTestClass {
   use Api3TestTrait;
 
   /**
+   * Setup for class.
+   */
+  public function setUp() {
+    parent::setUp();
+    $this->callAPISuccess('Setting', 'create', [
+      'deduper_resolver_bool_prefer_yes' => ['on_hold', 'do_not_email', 'do_not_phone', 'do_not_mail', 'do_not_sms', 'do_not_trade', 'is_opt_out'],
+    ]);
+  }
+
+  /**
    * Test the boolean resolver works.
    *
    * @throws \CiviCRM_API3_Exception
@@ -41,8 +51,7 @@ class CRM_DedupeTools_BAO_MergeConflictTest extends DedupeBaseTestClass {
    * Test that a boolean field is resolved if set.
    */
   public function testResolveBooleanFields() {
-    $this->ids['Contact'][0] = $this->callAPISuccess('Contact', 'create', ['first_name' => 'bob', 'do_not_mail' => 0, 'contact_type' => 'Individual'])['id'];
-    $this->ids['Contact'][1] = $this->callAPISuccess('Contact', 'create', ['first_name' => 'bob', 'do_not_mail' => 1, 'contact_type' => 'Individual'])['id'];
+   $this->createDuplicateIndividuals([['do_not_mail' => 0], ['do_not_mail' => 1]]);
     $this->callAPISuccess('Contact', 'merge', ['to_keep_id' => $this->ids['Contact'][0], 'to_remove_id' => $this->ids['Contact'][1]]);
     $mergedContacts = $this->callAPISuccess('Contact', 'get', ['id' => ['IN' => $this->ids['Contact']]])['values'];
 
@@ -59,6 +68,58 @@ class CRM_DedupeTools_BAO_MergeConflictTest extends DedupeBaseTestClass {
 
     $this->assertEquals(1, $mergedContacts[$this->ids['Contact'][2]]['contact_is_deleted']);
     $this->assertEquals(0, $mergedContacts[$this->ids['Contact'][0]]['contact_is_deleted']);
+  }
+
+  /**
+   * Test the boolean field resolver resolves emails on hold.
+   *
+   * @param bool $isReverse
+   *   Should we reverse which contact has on_hold set to true.
+   *
+   * @dataProvider booleanDataProvider
+   *
+   * @throws \CRM_Core_Exception
+   */
+  public function testResolveEmailOnHold($isReverse) {
+    $this->createDuplicateIndividuals();
+    // Conveniently our 2 contacts are 0 & 1 in the $this->ids['Contact'] array so we can abuse the boolean var like this.
+    $contactIDOnHold = $isReverse;
+
+    $email1 = $this->callAPISuccess('Email', 'get', ['contact_id' => $this->ids['Contact'][$contactIDOnHold]])['id'];
+    $this->callAPISuccess('Email', 'create', ['id' => $email1, 'on_hold' => 1]);
+
+    $mergeResult = $this->callAPISuccess('Contact', 'merge', ['to_keep_id' => $this->ids['Contact'][0], 'to_remove_id' => $this->ids['Contact'][1]])['values'];
+    $this->assertCount(1, $mergeResult['merged']);
+    $email0 = $this->callAPISuccessGetSingle('Email', ['contact_id' => ['IN' => [$this->ids['Contact'][0]]]]);
+    $this->assertEquals(1, $email0['on_hold']);
+  }
+
+  /**
+   * Data provider for tests with 2 options
+   *
+   * @return array
+   */
+  public function booleanDataProvider() {
+    return [[0], [1]];
+  }
+
+  /**
+   * Create individuals to dedupe.
+   *
+   * @param array $contactParams
+   *   Arrays of parameters.
+   */
+  private function createDuplicateIndividuals($contactParams = [[], []]) {
+    $params = [
+      'first_name' => 'bob',
+      'last_name' => 'Smith',
+      'contact_type' => 'Individual',
+      'email' => 'bob@example.com',
+    ];
+    foreach ($contactParams as $index => $contactParam) {
+      $contactParam = array_merge($params, $contactParam);
+      $this->ids['Contact'][$index] = $this->callAPISuccess('Contact', 'create', $contactParam)['id'];
+    }
   }
 
 }
