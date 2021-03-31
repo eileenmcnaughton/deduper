ede06e0381fb4e3ea3ded401059f1011d8de975f
eileen <emcnaughton@wikimedia.org>
2021-03-31 18:04:37 +1300

    Add name parsing api to deduper

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'f51b7fdb6413469bccb096e06d63e4e02ff43c01',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f51b7fdb6413469bccb096e06d63e4e02ff43c01',
+    ),
+    'theiconic/name-parser' => 
+    array (
+      'pretty_version' => 'v1.2.11',
+      'version' => '1.2.11.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9a54a713bf5b2e7fd990828147d42de16bf8a253',
+    ),
+  ),
+);
