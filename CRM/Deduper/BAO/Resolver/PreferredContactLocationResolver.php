cc77daa995fbb6791d122ace3910e396fe26a9ad
eileen <emcnaughton@wikimedia.org>
2024-08-04 21:35:39 +1200

    Tidy up from last fix

--- CRM/Deduper/BAO/Resolver/PreferredContactLocationResolver.php
+++ CRM/Deduper/BAO/Resolver/PreferredContactLocationResolver.php
@@ -84,7 +84,7 @@ class CRM_Deduper_BAO_Resolver_PreferredContactLocationResolver extends CRM_Dedu
         if ($this->isReHomingRequired($entity, $entitiesContactToDelete[$block], $block)) {
           $this->relocateLocation($entity, $block, FALSE, FALSE);
         }
-        foreach (array_keys($blockConflicts['fields'] ?? []) as $fieldName) {
+        foreach (array_keys($blockConflicts['fields']) as $fieldName) {
           // Keep the value from the contact to keep as that is preferred contact.
           $this->setResolvedLocationValue($fieldName, $entity, $block, $entitiesContactToKeep[$block][$fieldName]);
           if ($block === $primaryBlock && $entitiesContactToDelete[$block]['is_primary'] !== $entitiesContactToKeep[$block]['is_primary']) {
@@ -121,7 +121,7 @@ class CRM_Deduper_BAO_Resolver_PreferredContactLocationResolver extends CRM_Dedu
         if ($this->isReHomingRequired($entity, $blockConflicts['to_keep'], $block)) {
           $this->relocateLocation($entity, $block, TRUE, FALSE);
         }
-        foreach (array_keys($blockConflicts['fields'] ?? []) as $fieldName) {
+        foreach (array_keys($blockConflicts['fields']) as $fieldName) {
           // Keep the value from the contact to delete as that is preferred contact.
           $this->setResolvedLocationValue($fieldName, $entity, $block, $blockConflicts['to_remove'][$fieldName]);
         }
